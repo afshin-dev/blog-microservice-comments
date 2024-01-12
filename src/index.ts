3799b8f02de3a244917bb1f3e601092197669cee
afshin <shahinof10009@gmail.com>
2024-01-12 15:58:09 +0330

    add cors header with `cors` middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { HOST, PORT } from "./cfg/server.js";
 import Comments from "./routes/comments.js";
+import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/comments", Comments);
 
